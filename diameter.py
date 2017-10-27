e403cbc09358a8feaddbec0c17f318b865a0bc52
RicardoRei <ricardo.rei@ist.utl.pt>
2017-10-27 17:20:26 +0100

    diameter main module function

--- diameter.py
+++ diameter.py
@@ -3,12 +3,8 @@ import networkx as nx
 from BuildNetwork import load_network
 
 network = load_network()
-<<<<<<< HEAD
-def run():
-=======
 
-def main():
->>>>>>> be107558a76dee948bdaf732d19be1590ffa1f49
+def run():
 	diameter = nx.diameter(network)
 	print(diameter)
 

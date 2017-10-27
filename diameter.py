b21e7c08d6a1e61620c474982f5e13592cb91eec
Raquel Casteleiro <casteleiro.raquel@gmail.com>
2017-10-27 16:38:54 +0100

    diameter

---
+++ diameter.py
@@ -0,0 +1,16 @@
+import numpy as np
+import networkx as nx
+from BuildNetwork import load_network
+
+network = load_network()
+def main():
+	diameter = nx.diameter(network)
+	print(diameter)
+
+main()
+
+'''
+Diameter:
+
+Diameter is 3.
+'''

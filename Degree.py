937cdba1e45630348ebfe7e043ee857b052edd99
RicardoRei <ricardo.rei@ist.utl.pt>
2017-10-27 17:08:05 +0100

    degree with weights added

--- Degree.py
+++ Degree.py
@@ -1,7 +1,7 @@
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
-from BuildNetwork import load_network
+from build_network import load_network
 
 network = load_network()
 
@@ -28,30 +28,30 @@ The γ in this power law is ≈ 0.667. γ value is typically in the range 2 < γ
 """
     This Function receives the network graph and plots the degree histogram. 
 """
-def plotDegreeHistogram(network):
-    degree = nx.degree(network)
+def plot_weighted_degree_histogram(network):
+    degree = nx.degree(network, weight='weight')
     values = map(lambda x: x[1], degree)
     plt.hist(list(values))
-    plt.xlabel("Degree")
-    plt.ylabel("Nodes")
-    plt.title("Figure 1 -Degree distribution")
+    plt.xlabel("Weighted Degree")
+    plt.ylabel("Number of Nodes")
+    plt.title("Figure 1 - Weighted Degree distribution")
     plt.show()
-
 
 """
     This Function receives the network graph and plots the cumulative degree distribution.
     The Cumulative degree distribution cum_Pk represents the fraction of nodes with degree higher than k.
 """
-def plotCumulativeDist(network):
-    degree = nx.degree(network)
-    values = map(lambda x: x[1], degree)
+def plot_weighted_cumulative_dist(network):
+    degree = nx.degree(network, weight='weight')
+    values = list(map(lambda x: x[1], degree))
     max_degree = max(values)
+    values_range = len(values)
 
     cum_Pk = [0] * (max_degree + 50)  # + 50 so we can see the limit going to zero.
     k_values = [0] * (max_degree + 50)
     for k in range(0, max_degree + 50):
         k_values[k] = k
-        cum_Pk[k] = len([i for i in list(map(lambda x: x[1], degree)) if i >= k]) / len(network.nodes())
+        cum_Pk[k] = len([i for i in values if i >= k]) / values_range
 
     plt.xlabel("Cumulative Degree Distribution")
     plt.ylabel("Degree")
@@ -59,34 +59,13 @@ def plotCumulativeDist(network):
     plt.plot(k_values, cum_Pk)
     plt.show()
 
-
 """
-    This Function receives the network graph and plots the degree probability Pk.
-    The degree probability Pk represents the probability of a node has a degree  equal to k.
+    This Function receives the network graph and plots Cumulative Distribution of Contacts size or in other words the cumulative distribution
+    of the number of interactions.
+
+    The probability cum_Pk represents the probability of an individual has a interaction with size greater than k.
 """
-def plotDegreeProbability(network):
-    degree = nx.degree(network)
-    values = map(lambda x: x[1], degree)
-    max_degree = max(values)
-
-    Pk = [0] * (max_degree + 10)  # + 10 so we can see the limit going to zero.
-    k_values = [0] * (max_degree + 10)
-    for k in range(0, max_degree + 10):
-        k_values[k] = k
-        Pk[k] = len([i for i in list(map(lambda x: x[1], degree)) if i == k]) / len(network.nodes())
-
-    plt.xlabel("Degree")
-    plt.ylabel("Degree probability")
-    plt.title("Figure 3 - Degree Probability")
-    plt.plot(k_values, Pk, 'bo')
-    plt.show()
-
-
-"""
-    This Function receives the network graph and plots Cumulative Distribution of Contacts time.
-    The Contact Size probability Pk represents the probability of an individual has a contact of size k.
-"""
-def plotSizeOfContactsCumDist(network):
+def plot_interactions_cumulative_dist(network):
     weights = []
     for (u,v,w) in network.edges(data='weight'):
         weights.append(w)
@@ -100,14 +79,20 @@ def plotSizeOfContactsCumDist(network):
     
     plt.xlabel("Contact Size (Units: Interactions)")
     plt.ylabel("Cumulative Probability")
-    plt.title("Figure 4 - Cumulative Distribution of Contacts time")
+    plt.title("Figure 3 - Cumulative Distribution of Contacts time")
     data_points, = plt.plot(k_values, cum_Pk, 'bo', label='Data Points')
     powerLaw, = plt.plot([x**-0.667 for x in k_values if x != 0.0], 'k', label='Pk ≈ k^-γ')
     plt.legend(handles=[data_points, powerLaw])
     plt.show()
 
 
-def plotSizeOfContactsLogScale(network):
+
+"""
+    This Function receives the network graph and plots the Contacts size (number of interactions) in log scale.
+
+    The probability Pk represents the probability of an individual has a interaction with size k.
+"""
+def plot_interactions_number_log_scale(network):
     weights = []
     for (u,v,w) in network.edges(data='weight'):
         weights.append(w)
@@ -123,26 +108,15 @@ def plotSizeOfContactsLogScale(network):
     plt.xscale('log')
     plt.xlabel("Contact Size (Units: Interactions)")
     plt.ylabel("Probability")
-    plt.title("Figure 5 - Degree Probability")
+    plt.title("Figure 4 - Degree Probability")
     plt.plot(k_values, Pk, 'bo')
     plt.show()
 
 def main():
-	plotDegreeHistogram(network=network)  # figure 1
-	plotCumulativeDist(network=network)  # figure 2
-	# plotDegreeProbability(network=network) #NOT WHATS IS EXPECTED.... Power Law? # figure 3
-	plotSizeOfContactsCumDist(network=network)  # figure 4
-	plotSizeOfContactsLogScale(network=network)  # figure 5
-
-	degree = nx.degree(network)
-	degree_values = list(map(lambda x: x[1], degree))
-	average_degree = sum(degree_values) / len(degree_values)
-	print(average_degree)
-
-	degree = nx.degree(network, weight="weight")
-	degree_values = list(map(lambda x: x[1], degree))
-	average_weighted_degree = sum(degree_values) / len(degree_values)
-	print(average_weighted_degree)
+    plot_weighted_degree_histogram(network=network)
+    plot_weighted_cumulative_dist(network=network)
+    plot_interactions_cumulative_dist(network=network)
+    plot_interactions_number_log_scale(network=network)
 
 main()
 
